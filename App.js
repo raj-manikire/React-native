71de83edf7ca0d251a91319c31eb57996cefbd33
raj-manikire <rmnavigable@gmail.com>
2022-12-09 14:48:32 +0530

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import { View, Text } from "react-native";
-
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+import * as React from 'react';
+import { Text, View, StatusBar } from 'react-native';
+export default class App extends React.Component {
+  state = {
+    jsonData: '',
+  };
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          jsonData: json.body,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  render() {
+    return (
+      <View style={{ paddingTop: 30,margin:10, backgroundColor:'red' }}>
+        <Text>{this.state.jsonData}</Text>
+      </View>
+    );
+  }
 }
